6b709817aaa31f5647317deab8bf5badc83419e3
Christian <christianmorenoti@gmail.com>
2025-03-24 12:41:21 -0500

    Refactor main entry point to integrate Redux store and create custom hooks for dispatch and selector; add new index file for app rendering

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './app/store.ts'
-import App from './App.tsx'
-import './styles/index.css'
+import { store } from './store'
+import App from '../App'
+import '../styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </Provider>
   </React.StrictMode>,
-)
+) 
